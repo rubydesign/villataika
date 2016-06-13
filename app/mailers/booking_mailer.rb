e46e8acce5f464d3d1a139799d573e3a70410693
Torsten Ruger <torsten@villataika.fi>
2016-06-13 21:49:52 +0300

    add reply_to

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -2,6 +2,6 @@ class BookingMailer < ActionMailer::Base
 
   def confirm(booking)
     @booking  = booking
-    mail( :to => @booking.email , :subject => "#{t('booking.short')} #{booking.arriving}")
+    mail( :to => @booking.email ,:reply_to => @booking.email , :subject => "#{t('booking.short')} #{booking.arriving}")
   end
 end
