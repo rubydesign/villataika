acabbd59ac3fa50c32718b9ff7661ce1d851fbe6
Torsten Ruger <torsten@villataika.fi>
2018-06-10 07:32:42 +0300

    adds legacy redirect

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   root :controller => "page", :action => "index" , :locale => "fi"
 
+
+  get '/en/kartta.html',   to: redirect("/en/map.html")
+  get '/fi/kartta.html',   to: redirect("/fi/map.html")
+  get '/de/kartta.html',   to: redirect("/de/map.html")
+  
   scope "/:locale" do
     scope "villa" do
       resources :bookings
